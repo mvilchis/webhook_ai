862204e32090924190115aa0c8c61d184ee23823
mvilchis <mvilchis@ciencias.unam.mx>
2017-06-19 12:12:12 -0500

    Change kubernetes name

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
-while getopts ":p:u:" opt; do
+while getopts ":p:" opt; do
     case $opt in
         p) PROJECT_NAME="$OPTARG"
-           ;;
-        u) USERNAME="$OPTARG"
            ;;
         \?) echo "Opción inválida -$OPTARG" >&2
             ;;
@@ -18,15 +16,20 @@ git remote add "template" https://github.com/mvilchis/webhook-template.git
 
 git pull template master
 
-find ./ -type f -exec sed -i -e "s/-dummy/-${PROJECT_NAME}/g" {} \;
+########## CHANGE KUBERNETES FILE  ##########
+
+find ./kubernetes -type f -exec sed -i -e "s/-temporal/-${PROJECT_NAME}/g" {} \;
+mv kubernetes/webhook_deployment.yaml  kubernetes/webhook_${PROJECT_NAME}_deployment.yaml
+mv kubernetes/webhook_service.yaml  kubernetes/webhook_${PROJECT_NAME}_service.yaml
+
+########## END KUBERNETES FILE  #########
 
 
 echo ${PROJECT_NAME} > .project-name
 
-git add .project-name
+git add .project-name ./kubernetes
 
 git commit -m "Ajustando el nombre del proyecto a ${PROJECT_NAME}"
-
 
 echo "############################################################################################"
 echo "##                                                                                        ##"
@@ -35,5 +38,3 @@ echo "##                                                                        
 echo "############################################################################################"
 
 echo ""
-
-make info
